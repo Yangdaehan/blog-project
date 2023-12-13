d6d21aebde03affdfcb6927d1061610f37b3cd3b
Yangdaehan <yangdh08@naver.com>
2023-12-13 17:39:23 +0900

    feat        : 게시판 생성

---
+++ src/main/java/mse/blogproject/controller/api/BoardApiController.java
@@ -0,0 +1,41 @@
+package mse.blogproject.controller.api;
+
+import mse.blogproject.config.auth.PrincipalDetail;
+import mse.blogproject.dto.board.BoardSaveRequestDto;
+import mse.blogproject.dto.board.BoardUpdateRequestDto;
+import mse.blogproject.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class BoardApiController {
+
+    private final BoardService boardService;
+
+    /**
+     * 글작성 API
+     */
+    @PostMapping("/api/v1/board")
+    public Long save(@RequestBody BoardSaveRequestDto boardSaveRequestDto, @AuthenticationPrincipal PrincipalDetail principalDetail) {
+        return boardService.save(boardSaveRequestDto, principalDetail.getUser());
+    }
+
+    /**
+     * 글삭제 API
+     */
+    @DeleteMapping("/api/v1/board/{id}")
+    public Long deleteById(@PathVariable Long id) {
+        boardService.deleteById(id);
+        return id;
+    }
+
+    /**
+     * 글수정 API
+     */
+    @PutMapping("/api/v1/board/{id}")
+    public Long update(@PathVariable Long id, @RequestBody BoardUpdateRequestDto boardUpdateRequestDto) {
+        return boardService.update(id, boardUpdateRequestDto);
+    }
+}
---
+++ src/main/java/mse/blogproject/domain/board/Board.java
@@ -0,0 +1,49 @@
+package mse.blogproject.domain.board;
+
+import mse.blogproject.domain.BaseTimeEntity;
+import mse.blogproject.domain.reply.Reply;
+import mse.blogproject.domain.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Board extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    private String content;
+
+    @Column
+    private int count; //조회수
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @OrderBy("id desc")
+    @JsonIgnoreProperties({"board"})
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    private List<Reply> replyList;
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
